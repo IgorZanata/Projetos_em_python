d420b95008d7fcd6873581157d99aa8b49357c4c
Igor <igorzanata85@hotmail.com>
2026-08-25 11:36:42 -0300

    biblioteca pandas
    Usando bibliuoteca pandas para csv

--- Codigos_treinamento.py/SIstema_cadastro_alunos.py
+++ Codigos_treinamento.py/SIstema_cadastro_alunos.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Banco_dados:
     def __init__(self):
         self.usuarios = {}
@@ -8,83 +10,90 @@ class Banco_dados:
             print(f"RA: {chave} |Nome: {valor.nome} | Idade:{valor.idade}")
             print("===============")
 
-    
     def buscar(self,ra):
-
         if ra in self.usuarios:
             return self.usuarios[ra] 
-
         else:
             return None
         
+         
 class Pessoa:
     def __init__(self,ra,nome,idade):
         self.ra = ra
         self.nome = nome
         self.idade = idade
-
+        
+    def para_dicionario(self):
+        return {"RA" : self.ra, "NOME": self.nome, "IDADE" : self.idade }
+        
+    
 class Interface:
-    def __init__(self,banco):
-        self.banco = banco
+    def __init__(self):
+        self.banco = Banco_dados()
  
     def menu(self):
-        
         while True:
             try:
-
                 opcao = int(input("""
 [0] SAIR                         
 [1] ADICIONAR
 [2] EXIBIR
 [3] ALTERAR NOME
+[4] LER CSV
     --->>>"""))
                 
-                if opcao in [0,1,2,3]:
+                if opcao in [0,1,2,3,4]:
                     return opcao
-                
                 else:
                     print("Opcão invalida !\nSomente numeros entre [0 até 3]")
-                
             except ValueError:
                 print("Somente Numeros")
             
-
+    def salvar(self):
+        lista_dados = []
+        
+        for pessoa in self.banco.usuarios.values():
+            dicionario = pessoa.para_dicionario()
+            lista_dados.append(dicionario)
+            
+        escritor = pd.DataFrame(lista_dados)
+        escritor.to_csv("aula_006_dic.csv", sep=";", index=False, encoding="utf-8-sig")
+        print("Salvo")
+    
+    
+    def ler(self):
+        leitor = pd.read_csv("aula_006_dic.csv", sep=";", encoding="utf-8-sig")
+        print(leitor.to_string(index=False))
+        print("Finalizado")
+    
+    
+    
     def adicionar(self):
-
         try:
             ra = int(input("R.A:"))
-        
         except ValueError:
             print("Somente Numeros")
             return None
 
-
         if ra in self.banco.usuarios:
-
             print("Aluno Já cadastrado")
-        
         else:
-
             try:
                 nome = input("Nome:")
                 idade = int(input("Idade:"))
+                
                 if idade >= 0:
                     p1 = Pessoa(ra,nome,idade)
                     self.banco.usuarios[ra] = p1
-                    print("Cadastrado realizado com sucesso")
-                        
+                    self.salvar()  
                 else:
                     print("IDADE INVALIDA")
-
             except ValueError:
                 print("Somente Numeros")
 
-        
     def alterar_nome(self):
-
         try:
             ra_busca = int(input("Digite o R.A para encontrar\n--->>>"))
-        
         except ValueError:
             print("Somente Numeros")
             return None
@@ -95,34 +104,25 @@ class Interface:
             nome_atualizado = input("Digite o nome atualizado:")
             procurar.nome = nome_atualizado
             print("Nome atualizado com sucesso")
-
         else:
             print("Não encontrado")
-
-p1 = Banco_dados()  
-interacao = Interface(p1)
-
-while True:
-
-    opcao = interacao.menu()
-
-    if opcao == None:
-        continue
-
-    if opcao == 0:
-        print("Finalizando")
-        break
-
-    if opcao == 1:
-        interacao.adicionar()
-        
-    elif opcao == 2:
-        p1.exibir()
-    
-    elif opcao == 3:
-        interacao.alterar_nome()
-
-
-
-             
-    
+         
+    def menu_principal(self):      
+        while True:
+            opcao = self.menu()
+            if opcao == None:
+                continue
+            if opcao == 0:
+                print("Finalizando")
+                break
+            if opcao == 1:
+                self.adicionar()
+            elif opcao == 2:
+                self.banco.exibir()
+            elif opcao == 3:
+                self.alterar_nome()   
+            elif opcao == 4:
+                self.ler()    
+         
+interacao = Interface()
+interacao.menu_principal()
